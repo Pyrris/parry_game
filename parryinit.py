d8b63d59f0985a3d050fe355b4a65ae20e716ce8
pyrristitan@gmail.com <pyrristitan@gmail.com>
2025-03-15 12:57:55 +0800

    updated pygame.init

--- parryinit.py
+++ parryinit.py
@@ -14,6 +14,32 @@ class Player(pygame.sprite.Sprite):
         self.floor = 1000
         self.prev_y = self.rect.y
 
+        self.move_right = 0
+        self.move_left = 0
+        self.jump = 0
+        self.down = 0
+        self.attack = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+               
+        if keys[pygame.K_RIGHT]:
+            self.move_right = 1
+        if not keys[pygame.K_RIGHT]:
+            self.move_right = 0
+            
+        if keys[pygame.K_LEFT]:
+            self.move_left = 1
+        if not keys[pygame.K_LEFT]:
+            self.move_left = 0
+
+    def player_movement(self):
+
+        if self.move_right:
+            self.rect.x += 10
+        if self.move_left:
+            self.rect.x -=10
+
     def apply_gravity(self):
         self.prev_y = self.rect.y
         self.gravity += 1
@@ -22,27 +48,64 @@ class Player(pygame.sprite.Sprite):
             self.gravity = 100
 
     def airborne(self):
-        global airborne
         difference = self.rect.y - self.prev_y
+        print('difference = ' + str(difference))
         if difference == 0:
+            print('on ground')
             return False
         else:
+            print('in air')
             return True
 
-    def player_floor(self, y_pos):
-        self.floor = y_pos
-        if self.rect.bottom > self.floor:
-            self.rect.bottom = self.floor
-
     def collision(self, rect_list):
-        if self.airborne():
-            for rect in rect_list:
-                if self.rect.colliderect(rect):
-                    self.player_floor(rect.top)
-                    self.gravity = 0                  
+        for rect in rect_list:
+            if self.rect.colliderect(rect):
+                self.gravity = 0
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                # rel_xpos = 'right'
+                # rel_ypos = 'down'
+                #
+                # rel_x = rect.centerx - self.rect.centerx
+                # rel_y = rect.centery - self.rect.centery
+                # if rel_x >= 0: rel_xpos = 'right'
+                # else: rel_xpos = 'left'
+                # if rel_y >= 0: rel_ypos = 'down'
+                # else: rel_ypos = 'up'
+                #
+                # if rel_ypos == 'down':
+                #     if rel_xpos == 'right':
+                #         if rect.collidepoint(self.rect.bottomright):
+                #             self.rect.bottom = rect.top
+                #     if rel_xpos == 'left':
+                #         if rect.collidepoint(self.rect.bottomleft):
+                #             self.rect.bottom = rect.top
+                # if rel_xpos == 'right':
+                #     if rect.clipline((self.rect.right + 1, self.rect.bottom - 1), (self.rect.right + 1, self.rect.top + 1)):
+                #         self.rect.right = rect.left - 1
+                # if rel_xpos == 'left':
+                #     if rect.clipline((self.rect.left -1, self.rect.bottom -1), (self.rect.left -1, self.rect.top +1)):
+                #         self.rect.left = rect.right - 1
+
+
 
     def update(self):
         self.apply_gravity()
+        self.player_input()
+        self.player_movement()
+        self.collision(arena_rect_list)
+        self.airborne()
 
 # funcs
 
@@ -60,7 +123,7 @@ player.add(Player())
 
 
 # surf/rects
-floor_surf = pygame.Surface((1400, 100))
+floor_surf = pygame.Surface((1000, 100))
 floor_rect = floor_surf.get_rect(midbottom = (800, 800))
 arena_rect_list = [floor_rect]
 
@@ -81,7 +144,7 @@ while is_running:
     
     player.draw(screen)
     player.update()
-    player.sprite.collision(arena_rect_list)
+    print(player.sprite.move_left, player.sprite.move_right)
 
     # final updates
     pygame.display.update()
